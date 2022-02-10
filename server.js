899f15751fc04abbe08979c03a16b07d45dc97a1
Billal-Hossen <76785472+Billal-Hossen@users.noreply.github.com>
2022-02-10 23:08:30 +0600

    DB added

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose=require("mongoose");
 
 
 const DB=process.env.MONGODB_SERVER.replace('<PASSWORD>',process.env.DB_PASS)
-mongoose.connect(process.env.MONGODB_URL_LOCAL) 
+mongoose.connect(DB) 
 .then(()=>console.log("Connected to MongoDB"))
 .catch(err=>console.log("MongoDB Connecton Failed!!"));
  const port=process.env.PORT | 3005;
