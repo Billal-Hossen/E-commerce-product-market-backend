733ecfd4a98e3f5358b8e47bad48314bf1bd1d7b
Billal-Hossen <76785472+Billal-Hossen@users.noreply.github.com>
2022-02-10 18:59:46 +0600

    ready for heroku

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,15 +2,16 @@
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { urlencoded } = require('body-parser');
-const compression= require('compression')
+
+const compression= require('compression');
+const express = require('express');
 
 module.exports=app=>{
     //.........build in middleware.........
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.urlencoded( {extends:true} ))
+app.use(express.urlencoded({extended:true}))
 app.use(compression());
 if(process.env.NODE_ENV==="development"){
     app.use(morgan('dev'))
--- server.js
+++ server.js
@@ -2,10 +2,12 @@ require('dotenv/config');
 const app=require('./app');
 const mongoose=require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URL_LOCAL)
+
+const DB=process.env.MONGODB_SERVER.replace('<PASSWORD>',process.env.DB_PASS)
+mongoose.connect(DB) 
 .then(()=>console.log("Connected to MongoDB"))
 .catch(err=>console.log("MongoDB Connecton Failed!!"));
- const port=process.env.PORT || 3005;
+ const port=process.env.PORT | 3005;
 
  
 app.listen(port, ()=>console.log(`Listing on port ${port}.....`));
